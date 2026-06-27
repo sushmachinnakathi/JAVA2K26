13a08b6f849770baa72f04b23dd9e4faf175ca67
Sushma <sushma@gmail.com>
2026-06-27 14:47:14 +0530

    commiting the code and examples

---
+++ one.java
@@ -0,0 +1,10 @@
+public class one {
+    public static void main(String[] args) {
+        int num = 55; // Example number
+        if (num % 5 == 0) {
+            System.out.println("The number is divisible by 5");
+        } else {
+            System.out.println("The number is not divisible by 5");
+        }
+    }
+}
